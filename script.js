2607ae9143faf6fdb77f210765a9051f759bf78e
Juan Mata <jjmarrue@gmail.com>
2022-08-17 14:17:54 -0400

    Change logic to test chained operations

--- script.js
+++ script.js
@@ -12,10 +12,11 @@ const operations = document.querySelectorAll('.operator');
 // Define global variables
 
 display.textContent = "0";
-let num1 = 0;
-let total = num1;
+let num1 = null;
+let total = 0;
 let num2 = 0;
 let operatorVal = null;
+let tempNum = null;
 
 // Operations 
 
@@ -24,18 +25,18 @@ function addTwoNumbers(num1, num2) {
 }
 
 function substractTwoNumbers(num1, num2) {
-  return num1 - num2;
+  return +num1 - +num2;
 }
 
 function multiplyTwoNumbers(num1, num2) {
-  return num1 * num2;
+  return +num1 * +num2;
 }
 
 function divideTwoNumbers(num1, num2) {
   if (num2 === 0) {
     return 'LOL';
   }
-  return num1 / num2;
+  return +num1 / +num2;
 }
 
 
@@ -57,57 +58,125 @@ function operate(operator, num1, num2) {
   }
 }
 
-//console.log(operate(recordOperation(), -3.6, 5));
 
+let lastClickedBtn;
+let currentClickedBtn;
+const allBtns = document.querySelectorAll('.btn');
 
-// function displayValue(num) {
-//   if (display.textContent === '0'|| recordOperation() === true) {
-//     display.textContent = num.value;
-//   } else {
-//     display.textContent += num.value;
-//   }
-// }
+window.addEventListener('DOMContentLoaded', trackClickedButtons);
 
-
-numbers.forEach(num => {
-  num.addEventListener('click', (e) => {
-    let lastPressedVal = num.value;
-    if (operatorVal == null) {
-      if (num1 === 0) {
-        num1 = lastPressedVal;
-      } else {
-        num1 += lastPressedVal;
+function trackClickedButtons() {
+  allBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+      lastClickedBtn = currentClickedBtn;
+      currentClickedBtn = btn.value;
+      //console.log(`last button: ${lastClickedBtn}`);
+      //console.log(`current button: ${currentClickedBtn}`);
+       
+      if (btn.classList.contains('number')) {
+        if (tempNum === null) {
+          tempNum = currentClickedBtn;
+        } else {
+          tempNum += currentClickedBtn;
+          
+        }
+        console.log(`num on screen: ${tempNum}`);     
       }
-      total = num1;
       
-    } else {
-      if (num2 === 0 || num2 == null) {
-        num2 = lastPressedVal;
-      } else {
-        num2 += lastPressedVal;
-      }           
-    }
-    console.log(`num1: ${num1}`);
-    console.log(`num2: ${num2}`);
-  });
-});
+      if (btn.classList.contains('operator')) {
+        if (tempNum != null) {
+          num1 = tempNum;
+        } else {
+          num2 = tempNum;
+          num1 = total;
+          
+        }
 
-operations.forEach(operator => {
-  operator.addEventListener('click', (e) =>{
-    operatorVal = operator.value;
-    total = operate(operatorVal, num1, num2);
-    num1 = total; //first number now holds the current total
-    num2 = null;  //reset second number so user can set a new value
+      
+      operatorVal = currentClickedBtn; 
 
-    console.log(`total upon operation: ${total}`); 
+      //console.log(`operator on screen: ${operatorVal}`);
+      
+      total = operate(operatorVal, num1, num2);
+      console.log(`num1: ${num1}`);
+      console.log(`num2: ${num2}`); 
+      console.log(`total: ${total}`); 
+      tempNum = null;
+      }
+    
+    
+    });
+    
   });
-});
 
 
   equals.addEventListener('click', (e) => {
+    num2 = tempNum;
     total = operate(operatorVal, num1, num2);
-    num1 = total; //first number now holds the current total
+    console.log(`num1: ${num1}`);
+    console.log(`num2: ${num2}`); 
     console.log(`equals: ${total}`); 
+    
+    num1 = total; //first number now holds the current total
+    tempNum = null;
   });
+}
+
+
+
+
+
+function obtainNumber() {
+  numbers.forEach(num => {
+    num.addEventListener('click', (e) => {
+      return num.value;
+    });
+  });
+}
+
+
+// numbers.forEach(num => {
+//   num.addEventListener('click', (e) => {
+//     lastPressedVal = num.value;
+//     if (operatorVal == null) {
+//       if (num1 === 0) {
+//         num1 = lastPressedVal;
+//       } else {
+//         num1 += lastPressedVal;
+//       }
+//       total = num1;
+      
+//     } else {
+//       if (num2 === 0 || num2 == null) {
+//         num2 = lastPressedVal;
+//       } else {
+//         num2 += lastPressedVal;
+//       }           
+//     }
+//     console.log(`num1: ${num1}`);
+//     console.log(`num2: ${num2}`);
+//   });
+// });
+
+
+
+
+
+// operations.forEach(operator => {
+//   operator.addEventListener('click', (e) =>{
+//     operatorVal = operator.value;
+//     console.log(operatorVal);
+     
+//     //console.log(`total upon operation: ${total}`); 
+//     num1 = total; //first number now holds the current total
+//     num2 = null;  //reset second number so user can set a new value
+//   });
+// });
+
+
+
+
+
+
 
 
