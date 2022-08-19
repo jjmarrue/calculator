c0fc551f297ed9b0f3c030e37e5df567e18f7a1f
Juan Mata <jjmarrue@gmail.com>
2022-08-19 11:13:56 -0400

    Adjust how numbers are displayed
    - Change code to allow computation to be displayed above results
    - Implement decimal functionality
    - Implement 8 decimal limit
    - Add logic to clear button

--- script.js
+++ script.js
@@ -4,43 +4,47 @@ const clear = document.getElementById('clear');
 const percent = document.getElementById('percentage');
 const plusminus = document.getElementById('plusminus');
 const decimal = document.getElementById('decimal');
-const equals = document.getElementById('result');
+const equals = document.getElementById('equals');
 const display = document.querySelector('#display span');
+const calculation = document.getElementById('calculation');
+const result = document.getElementById('result');
 const numbers = document.querySelectorAll('.number');
 const operations = document.querySelectorAll('.operator');
+const allBtns = document.querySelectorAll('.btn');
 
 // Define global variables
 
-display.textContent = "0";
 let num1 = null;
-let total = 0;
 let num2 = null;
-let operatorVal = null;
+let total = 0;
 let tempNum = null;
-
-// Operations 
+let operatorVal = null;
+let previousOperator;
+let currentClickedBtn;
+const decimalPlaces = 8;
+// Functions for each operation 
 
 function addTwoNumbers(num1, num2) {
-  return +num1 + +num2;
+  return (+num1 + +num2).toFixed(decimalPlaces);
 }
 
 function substractTwoNumbers(num1, num2) {
-  return +num1 - +num2;
+  return (+num1 - +num2).toFixed(decimalPlaces);
 }
 
 function multiplyTwoNumbers(num1, num2) {
-  return +num1 * +num2;
+  return (+num1 * +num2).toFixed(decimalPlaces);
 }
 
 function divideTwoNumbers(num1, num2) {
   if (num2 == 0) {
     return 'LOL';
   } 
-    return +num1 / +num2;
+    return (+num1 / +num2).toFixed(decimalPlaces);
  
 }
 
-// Use correct operator on numbers
+// Use correct operator on numbers based on operator value
 
 function operate(operator, num1, num2) {
   if (operator === '-') {
@@ -57,10 +61,7 @@ function operate(operator, num1, num2) {
   }
 }
 
-
-let previousOperator;
-let currentClickedBtn;
-const allBtns = document.querySelectorAll('.btn');
+// Run the function as soon as the HTML document finishes loading
 
 window.addEventListener('DOMContentLoaded', trackClickedButtons);
 
@@ -68,7 +69,12 @@ function trackClickedButtons() {
   allBtns.forEach(btn => {
     btn.addEventListener('click', e => {
 
+      // Keep track of the last operator so it can be used later
+
       previousOperator = operatorVal;
+
+      // Keep track of the key that was just pressed
+
       currentClickedBtn = btn.value;
       
       // NUMBERS
@@ -76,16 +82,20 @@ function trackClickedButtons() {
       if (btn.classList.contains('number')) {
         if (tempNum === null) {
           tempNum = currentClickedBtn;
+          result.textContent = tempNum;
         } else {
           tempNum += currentClickedBtn;
+          result.textContent = tempNum;
         }
         console.log(`num on screen: ${tempNum}`);     
       }
       
       // OPERATORS
 
-      if (btn.classList.contains('operator')) {
+      if (btn.classList.contains('operator') || btn.classList.contains('equals')) {
         operatorVal = currentClickedBtn;
+        calculation.textContent += tempNum;
+        calculation.textContent += operatorVal;
         console.log(`current operator: ${operatorVal}`);
 
         if (num1 === null) {
@@ -96,36 +106,48 @@ function trackClickedButtons() {
           tempNum = null;
         }
        
+        // To allow chain operations, perform each operation using the value of the previous operator
+
         if (previousOperator) {
           console.log(`previous operator: ${previousOperator}`); 
-        console.log(`num1: ${num1}`);
-        console.log(`num2: ${num2}`);
-        total = operate(previousOperator, num1, num2);
-        console.log(`total: ${total} = ${num1} ${previousOperator} ${num2}`);
-        num1 = parseFloat(total);
+          console.log(`num1: ${num1}`);
+          console.log(`num2: ${num2}`);
+          total = operate(previousOperator, num1, num2);    
+          console.log(`total: ${total}`);
+
+          num1 = total;
         }
-    
-        //total = operate(previousOperator, num1, num2);
-        // console.log(`num1: ${num1}`);
-        // console.log(`num2: ${num2}`);
-        //console.log(`total: ${total} = ${num1} ${previousOperator} ${num2}`);
-        //num1 = total;
     }
-   
-    //
-    
   });    
 });
-
-
-  // equals.addEventListener('click', (e) => {
-  //   num2 = tempNum;
-  //   total = operate(operatorVal, num1, num2);
-  //   console.log(`num1: ${num1}`);
-  //   console.log(`num2: ${num2}`); 
-  //   console.log(`equals: ${total}`); 
-    
-  //   num1 = total; //first number now holds the current total
-  //   //console.log(`num1: ${num1}`);
-  // });
 }
+
+// Pressing equals handles variables a little differently
+
+equals.addEventListener('click', (e) => {
+  if (num1 !== null) {
+    num2 = tempNum;
+    total = operate(previousOperator, num1, num2);
+    console.log(`num1: ${num1}`);
+    console.log(`num2: ${num2}`);
+    console.log(`equals: ${total}`);
+    result.textContent = total;
+    calculation.textContent = num1 + previousOperator + num2;
+    num1 = total; //first number now holds the current total
+    num2 = null;
+    operatorVal = null;
+  }
+});
+
+// Clear everything
+
+clear.addEventListener('click', (e) => {
+  num1 = null;
+  num2 = null;
+  total = 0;
+  tempNum = null;
+  operatorVal = null;
+  previousOperator = null;
+  result.textContent = '0';
+  calculation.textContent = '';
+});
