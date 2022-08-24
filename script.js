9bc5653e842376ba677308cfb15bed9d79fb44ac
Juan Mata <jjmarrue@gmail.com>
2022-08-24 15:20:51 -0400

    Allow keyboard inputs
    - Add two functions that manage click and keyboard inputs.
    - Only numbers, operators, esc, and backspace keys are allowed as inputs.
    - Move core logic to new function.
    - Change values for %, backspace, and = buttons on index.html to accomodate for keyboard inputs.

--- script.js
+++ script.js
@@ -9,18 +9,21 @@ const display = document.querySelector('#display span');
 const numbers = document.querySelectorAll('.number');
 const operations = document.querySelectorAll('.operator');
 const allBtns = document.querySelectorAll('.btn');
+const buttons = document.querySelector(".container");
 
 // Define global variables
 
 let num1 = null;
 let num2 = null;
 let total = 0;
-let tempNum = null;
+let tempNum = '';
 let operatorVal = null;
 let previousOperator;
 let currentClickedBtn;
 const decimalPlaces = 10;
 let percentVal = null;
+const maxDigits = 10;
+const fractionDigits = 8;
 
 // Use number.EPSILON to provide accurate rounding
 
@@ -29,7 +32,27 @@ Number.prototype.round = function(n) {
   return Math.round((this + Number.EPSILON) * d) / d;
 }
 
-// Functions for each operation. parseFloat removes trailing zeros.
+const limitDisplay = function(n) {
+  if (n !== null) {
+    n = n.toString();
+    if (n.length > maxDigits) {    
+      n = n.substring(0, maxDigits);
+    }   
+    return n;
+  } else {
+    return n;
+  }  
+}
+
+const convertToScientificNotation = function(n) {
+  n = n.toString();
+  if (n.length > maxDigits) {
+    return Number.parseFloat(n).toExponential(fractionDigits);
+  }
+  else {
+    return n;
+  }
+}
 
 function addTwoNumbers(num1, num2) {
   return parseFloat((+num1 + +num2).round(decimalPlaces));
@@ -54,187 +77,165 @@ function getPercentage(num1, num2, percentVal) {
   return  parseFloat(((+num2/100)*(+num1)).toFixed(decimalPlaces));
 }
 
+
 // Use correct operator on numbers based on operator value
 
 function operate(operator, num1, num2) {
-  if (operator === '-') {
+  if (operator == '-') {
     return substractTwoNumbers(num1, num2);
   }
-  if (operator === '+') {
+  if (operator == '+') {
     return addTwoNumbers(num1, num2);
   }
-  if (operator === 'x') {
+  if (operator == '*') {
     return multiplyTwoNumbers(num1, num2);
   }
-  if (operator === '÷') {
+  if (operator == '/') {
     return divideTwoNumbers(num1, num2);
   }
 }
 
-// Run the function as soon as the HTML document finishes loading
 
-window.addEventListener('DOMContentLoaded', trackClickedButtons);
+const calculatorAction = (e) => {
+  console.log(e);
+  previousOperator = operatorVal;
 
-function trackClickedButtons() {
-  allBtns.forEach(btn => { 
-    btn.addEventListener('click', e => {
+  // Handle backspace
 
-      // Keep track of the last operator so it can be used in the calculation
+  if (e == 'Backspace') {
+    const arrayofNums = Array.from(String(tempNum), Number);
+    arrayofNums.pop();
+    tempNum = arrayofNums.join('');
+    if (tempNum.length === 0) {
+      tempNum = 0;
+    }
+    display.textContent = tempNum;
+  }
 
-      previousOperator = operatorVal;
+  if (!(isNaN(e))) {     
+    if (tempNum == null || tempNum == 0) {
+      tempNum = e;
+      tempNum = limitDisplay(tempNum);
+      display.textContent = tempNum;   
+    } else {
+      tempNum += e;
+      tempNum = limitDisplay(tempNum);
+      display.textContent = tempNum;
+    }
+    console.log(tempNum);
+  }
 
-      // Keep track of the key that was just pressed
+  if (e == '%') {
+    percentVal = e; 
+  }
+  
+  if (e == '/' || e == '*' || e == '+' || e == '-') {
+    operatorVal = e;
+    if (tempNum === null && num1 == null) {
+      num1 = 0;
+    } else if (tempNum === null && num1 !== null) {
+      tempNum = 0;
+      display.textContent += tempNum;
+    } 
+    if (num1 === null) {
+      num1 = tempNum;
+      tempNum = null;
+    } else {  
+      if(percentVal !== null) {
+        console.log(num1);
+        console.log(tempNum);
+        num2 = getPercentage(num1, tempNum, percentVal);
+      } else {
+        num2 = tempNum;
+      }
+      tempNum = null;
+    }
+    
+    // Chanined operations
 
-      currentClickedBtn = btn.value;
-      
-      // DELETE CHARACTER FROM NUMBER
+    if (previousOperator && num2 !== null) {
+      total = operate(previousOperator, num1, num2);
+      total = convertToScientificNotation(total);
+      display.textContent = total;
+      num1 = total;
+      num2 = null;
+    }
+  }
 
-      if (btn.id === 'backspace') {
-        const arrayofNums = Array.from(String(tempNum), Number);
-        arrayofNums.pop();
-        tempNum = arrayofNums.join('');
-        if (tempNum.length === 0) {
-          tempNum = 0;
-        }
+  if (e == '.') {
+    if (display.textContent.indexOf('.') === -1) {
+      if (tempNum === null) {
+        tempNum = e;
+        display.textContent = tempNum;   
+      } else {
+        tempNum += e;
         display.textContent = tempNum;
       }
+    } else {
+      return false;
+    }
+  }
 
-      //POSITIVE - NEGATIVE
-
-      if (btn.id === 'plusminus') {
-        if (total !== 0) {
-          total = -total;
-          display.textContent = total; 
-        }
-        else {
-          tempNum = -tempNum;
-          display.textContent = tempNum; 
-        }
-        //num2 = null;
-        operatorVal = null;
+  if (e == 'Enter') {
+    if (tempNum === null) {
+      tempNum = 0;
+    }
+    if (num1 !== null && previousOperator !== null) {
+      if(percentVal !== null) {
+        num2 = getPercentage(num1, tempNum, percentVal);
+      } else {
+        num2 = tempNum;
       }
+      total = operate(previousOperator, num1, num2);
+      console.log(num2);
+      display.textContent = total;
+      percentVal = null;
+    } else {
+      display.textContent = total;
+    }
+    num1 = total;
+    console.log('num1: ' + num1);
+    console.log('num2: ' + num2);
+    console.log('total: ' + total);
+  }
 
+  if (e == 'Escape') {
+    num1 = null;
+    num2 = null;
+    total = 0;
+    tempNum = null;
+    operatorVal = null;
+    previousOperator = null;
+    percentVal = null;
+    display.textContent = '0';
+  }
 
-      // PERCENTAGE
+  if (e == '+/-') {
+    if (total !== 0) {
+      total = -total;
+      display.textContent = total; 
+    }
+    else {
+      tempNum = -tempNum;
+      display.textContent = tempNum; 
+    }
+    operatorVal = null;
+  }
 
-      if (btn.id === 'percentage') {
-        percentVal = btn.value;       
-      }
-
-      // DECIMAL
-
-      if (btn.id === 'decimal') {
-        if (display.textContent.indexOf('.') === -1) {
-          if (tempNum === null) {
-            tempNum = currentClickedBtn;
-            display.textContent = tempNum;   
-          } else {
-            tempNum += currentClickedBtn;
-            display.textContent = tempNum;
-          }
-				} else {
-					return false;
-				}
-      }
-
-      // NUMBERS
-
-      if (btn.classList.contains('number')) {
-
-        if (tempNum === null) {
-          tempNum = currentClickedBtn;
-          display.textContent = tempNum;   
-        } else {
-          tempNum += currentClickedBtn;
-          display.textContent = tempNum;
-        }
-      }
-    
-      // OPERATORS
-
-      if (btn.classList.contains('operator') || btn.classList.contains('equals')) {
-        console.log('operator was pressed');
-        console.log('previousOp: ' + previousOperator);
-        //tempNum = null;
-        operatorVal = currentClickedBtn;
-       
-        // if the first number has not been entered and user presses an operator, it should go 0 [operator] [num2]
-
-        if (tempNum === null && num1 == null) {
-          num1 = 0;
-        } else if (tempNum === null && num1 !== null) {
-          tempNum = 0;
-          display.textContent += tempNum;
-        } 
- 
-        if (num1 === null) {
-          num1 = tempNum;
-          tempNum = null;
-        } else {  
-          if(percentVal !== null) {
-            num2 = getPercentage(num1, tempNum, percentVal);
-            console.log('Percentage: ' + num2);
-          } else {
-            num2 = tempNum;
-          }
-          console.log('num2: ' + num2);
-          tempNum = null;
-        }
-       
-        // To allow chain operations, perform each operation using the value of the previous operator and put total value in num1
-
-        if (previousOperator && num2 !== null) {
-          total = operate(previousOperator, num1, num2);
-          display.textContent = total;
-          num1 = total;
-          num2 = null;
-        }
-        // console.log('num1: ' + num1);
-        // console.log('num2: ' + num2);
-        // console.log('total: ' + total); 
-    } 
-  });   
-});
 }
 
-// Pressing equals handles variables a little differently
+buttons.addEventListener('click', (e) => {
+  const buttonValue = (e.target.value);
+  calculatorAction(buttonValue);
+})
 
-equals.addEventListener('click', (e) => {
-  console.log('equals was pressed');
-  if (tempNum === null) {
-    tempNum = 0;
+
+
+document.addEventListener('keydown', function(e) {
+  let eventKey = (e.key) ? e.key : KeyboardEvent.keyCode;
+  if (eventKey >= 0 && eventKey <= 9 || eventKey == 'Enter' || eventKey == '%' || eventKey == 'Escape' || eventKey == '/' || eventKey == '*' || eventKey == '-' || eventKey == '+' || eventKey == '=' || eventKey == '.') {
+    calculatorAction(e.key);   
   }
-  if (num1 !== null && previousOperator !== null) {
-    if(percentVal !== null) {
-      num2 = getPercentage(num1, tempNum, percentVal);
-    } else {
-      num2 = tempNum;
-    }
-    total = operate(previousOperator, num1, num2);
-    console.log(num2);
-    display.textContent = total;
-    percentVal = null;
-  } else {
-    display.textContent = total;
-  }
-  num1 = total;
-  //operatorVal = null;
-  //console.log(previousOperator);
-  //tempNum = null;
-  console.log('num1: ' + num1);
-  console.log('num2: ' + num2);
-  console.log('total: ' + total);
 });
 
-// Clear everything
 
-clear.addEventListener('click', (e) => {
-  num1 = null;
-  num2 = null;
-  total = 0;
-  tempNum = null;
-  operatorVal = null;
-  previousOperator = null;
-  percentVal = null;
-  display.textContent = '0';
-});
